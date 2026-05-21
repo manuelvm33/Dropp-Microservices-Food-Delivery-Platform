49159aa6855f7b5062d7b3820f654ee5430d6ce3
manuelvm33 <elkin.manuelvm@gmail.com>
2026-05-21 18:12:10 -0500

    feat(): local deployment docker compose added and configuration for local deployment

---
+++ frontend/dropp/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    api: {
+        restaurant: 'EC2_HOST_PLACEHOLDER/restaurant-listing-service',
+        order: 'EC2_HOST_PLACEHOLDER/order-service',
+        foodCatalog: 'EC2_HOST_PLACEHOLDER/food-catalogue-service',
+        user: 'EC2_HOST_PLACEHOLDER/user-service'
+    }
+};
