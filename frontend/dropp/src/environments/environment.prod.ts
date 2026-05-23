b82f11b14b39fec6cdc022709a5ad53fdb2bbc21
manuelvm33 <elkin.manuelvm@gmail.com>
2026-05-23 11:58:45 -0500

    fix: change api-gateway port to 8081

--- frontend/dropp/src/environments/environment.prod.ts
+++ frontend/dropp/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
     api: {
-        restaurant: 'EC2_HOST_PLACEHOLDER/restaurant-listing-service',
-        order: 'EC2_HOST_PLACEHOLDER/order-service',
-        foodCatalog: 'EC2_HOST_PLACEHOLDER/food-catalogue-service',
-        user: 'EC2_HOST_PLACEHOLDER/user-service'
+        restaurant: 'EC2_HOST_PLACEHOLDER:8081/restaurant-listing-service',
+        order: 'EC2_HOST_PLACEHOLDER:8081/order-service',
+        foodCatalog: 'EC2_HOST_PLACEHOLDER:8081/food-catalogue-service',
+        user: 'EC2_HOST_PLACEHOLDER:8081/user-service'
     }
 };
