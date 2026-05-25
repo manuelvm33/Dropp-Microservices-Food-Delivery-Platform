1758f957959f3b9f689697daa074743d4b91cec4
manuelvm33 <elkin.manuelvm@gmail.com>
2026-05-24 20:12:54 -0500

    fix: update frontend port

--- frontend/dropp/src/environments/environment.prod.ts
+++ frontend/dropp/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
     api: {
-        restaurant: 'EC2_HOST_PLACEHOLDER:8081/restaurant-service/restaurant',
-        order: 'EC2_HOST_PLACEHOLDER:8081/order-service/order',
-        foodCatalog: 'EC2_HOST_PLACEHOLDER:8081/food-catalogue-service/foodCatalog',
-        user: 'EC2_HOST_PLACEHOLDER:8081/user-service/user'
+        restaurant: 'EC2_HOST_PLACEHOLDER:8081/restaurant-service',
+        order: 'EC2_HOST_PLACEHOLDER:8081/order-service',
+        foodCatalog: 'EC2_HOST_PLACEHOLDER:8081/food-catalogue-service',
+        user: 'EC2_HOST_PLACEHOLDER:8081/user-service'
     }
 };
